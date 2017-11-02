ce30b80cac29a76cab41239afdaa56265b3474ba
Vladimir <nikonov.volodymyr@gmail.com>
2017-11-02 13:54:44 +0200

    исправление ошибки в режиме разработки через VS

--- JavaScriptOnlineLoader/Schemas/ClientFileWather/ClientFileWather.js
+++ JavaScriptOnlineLoader/Schemas/ClientFileWather/ClientFileWather.js
@@ -6,22 +6,8 @@ define("ClientFileWather", [], function() {
 				if (message.Header.Sender === "FileWatcher") {
 					var moduleName = Ext.decode(message.Body);
 					Terrasoft.each(require.s.contexts._.defined, function(module, dm) {
-						if (dm.indexOf(moduleName) > -1) {
-							var descriptor = core.getModuleDescriptor(dm);
-							if (descriptor) {
-								var path = descriptor.path;
-								var match = path.split("/");
-								if (match && match[1]) {
-									match = match[1];
-									path = path.replace(match, Terrasoft.generateGUID().replace(/-/gi, ""));
-								}
-								descriptor.path = path;
-								core.setModuleDescriptor(dm, descriptor);
-							}
-							require.undef(dm);
-						}
-						require([moduleName]);
-					});
+						this.prepareDefinedModuleDescriptor(moduleName, dm);
+					}, this);
 					var cmp = Ext.getCmp("mainContentWrapper");
 					if (cmp && cmp.destroyed !== true){
 						cmp.destroy();
@@ -29,6 +15,25 @@ define("ClientFileWather", [], function() {
 					core.loadModule("ViewModuleWrapper");
 				}
 			}, this);
+		},
+
+		prepareDefinedModuleDescriptor: function(moduleName, dm) {
+			if (dm.indexOf(moduleName) > -1) {
+				var descriptor = core.getModuleDescriptor(dm);
+				if (descriptor) {
+					var path = descriptor.path;
+					var match = path.split("/");
+					var length = match && match.length;
+					if (match && match[length - 1]) {
+						match = match[length - 1];
+						path = path.replace(match, Terrasoft.generateGUID().replace(/-/gi, ""));
+					}
+					descriptor.path = path;
+					core.setModuleDescriptor(dm, descriptor);
+				}
+				require.undef(dm);
+			}
+			require([moduleName]);
 		}
 	});
 	return Ext.create("Terrasoft.ClientFileWather", {});
