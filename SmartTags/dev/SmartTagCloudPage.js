f4c784989ec666b9852a9f16719f98811a5d7a23
Dmytro <theca@ukr.net>
2017-12-04 12:38:24 +0200

    #cloud updated
    #cloud updated

--- SmartTags/dev/SmartTagCloudPage.js
+++ SmartTags/dev/SmartTagCloudPage.js
@@ -7,7 +7,7 @@ define("SmartTagsCloudPage", ["SmartTagsD3LayoutCloud"], function() {
 				collection.each(function (item) {
 					result.push({
 						"text": item.get("Tag.Name"),
-						"size": item.get("Count") * 10
+						"size": item.get("Count") * 15
 					});
 				}, this);
 				return result;
@@ -29,34 +29,54 @@ define("SmartTagsCloudPage", ["SmartTagsD3LayoutCloud"], function() {
 			},
 			
 			_drawTagCloud: function (tags) {
-				var color = d3.scale.linear()
-					.domain([0,1,2,3,4,5,6,10,15,20,100])
-					.range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
-				d3.select("#PNGTagCloudPageTagCloudContainer")
-					.append("svg")
-					.attr("width", 800)
-					.attr("height", 300)
+				var width = 800;
+				var height = 300;
+				var fill = d3.scale.category20();
+				d3.select("#PNGTagCloudPageTagCloudContainer").append("svg")
+					.attr("width", width)
+					.attr("height", height)
 					.append("g")
-					// without the transform, words words would get cutoff to the left and top, they would
-					// appear outside of the SVG area
-					.attr("transform", "translate(320,200)")
+					.attr("transform", "translate(" + ~~(width / 2) + "," + ~~(height / 2) + ")")
 					.selectAll("text")
 					.data(tags)
 					.enter().append("text")
-					.style("font-size", function(d) { return d.size + "px"; })
-					.style("fill", function(d, i) { return color(i); })
+					.style("font-size", function(d) {
+						return d.size + "px";
+					})
+					.style("-webkit-touch-callout", "none")
+					.style("-webkit-user-select", "none")
+					.style("-khtml-user-select", "none")
+					.style("-moz-user-select", "none")
+					.style("-ms-user-select", "none")
+					.style("user-select", "none")
+					.style("cursor", "default")
+					.style("font-family", "Impact")
+					.style("fill", function(d, i) {
+						return fill(i);
+					})
+					.attr("text-anchor", "middle")
 					.attr("transform", function(d) {
 						return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
 					})
-					.text(function(d) { return d.text; });
+					.text(function(d) {
+						return d.text;
+					});
 			},
 			
 			_rendered: function() {
 				this._requestData(function (tagCloudData) {
-					d3.layout.cloud().size([800, 300])
+					var width = 800;
+					var height = 300;
+					d3.layout.cloud()
+						.size([width, height])
 						.words(tagCloudData)
-						.rotate(0)
-						.fontSize(function(d) { return d.size; })
+						.rotate(function() {
+							return ~~(Math.random() * 2) * 90;
+						})
+						.font("Impact")
+						.fontSize(function(d) {
+							return d.size;
+						})
 						.on("end", this._drawTagCloud)
 						.start();
 				}, this);
