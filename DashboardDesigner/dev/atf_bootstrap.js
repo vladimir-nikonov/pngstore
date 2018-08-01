d64fb0c44cb1f4cc917ded4809cd0ed6a1357ce5
Dmytro <theca@ukr.net>
2018-08-01 11:27:59 +0300

    13
    bootstrap fix

--- DashboardDesigner/dev/atf_bootstrap.js
+++ DashboardDesigner/dev/atf_bootstrap.js
@@ -3,7 +3,6 @@ requirejs.config({
 		atf_dashboarddesigner: "https://pngstore.azureedge.net/DashboardDesigner/src/build/atf_dashboarddesigner-min",
 		atf_dashboarddesigner_overrides: "https://pngstore.azureedge.net/DashboardDesigner/src/build/atf_dashboarddesigner_overrides-min"
 	},
-	waitSeconds: 300,
 	shim: {
 		atf_dashboarddesigner: {
 			deps: ["DashboardManager", "DashboardDesignerViewGenerator", "CommonCSSV2", "PreviewableGridLayoutEdit",
