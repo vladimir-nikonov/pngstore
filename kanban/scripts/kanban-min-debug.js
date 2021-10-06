acdec9156140b332f22fe91c16c93fc4a805a37b
Vladimir <nikonov.volodymyr@gmail.com>
2021-10-06 10:43:09 +0300

    disable kanban changes

--- kanban/scripts/kanban-min-debug.js
+++ kanban/scripts/kanban-min-debug.js
@@ -1525,7 +1525,7 @@ define("KanbanSection", ["PageUtilities", "ConfigurationEnums"], function(PageUt
 				this.callParent([function() {
 					this.set("DcmCases", this.Ext.create("Terrasoft.Collection"));
 					Kanban = {
-						HideImage: this.getIsFeatureEnabled("EnableKanbanForActivitySection")
+						HideImage: this.getIsFeatureEnabled("EnableKanbanForActivitySection") && false
 					};
 					this._initKanbanStorage();
 					this._loadKanbanProfile(callback, scope);
@@ -1790,7 +1790,7 @@ define("KanbanSection", ["PageUtilities", "ConfigurationEnums"], function(PageUt
 			},
 
 			enableKanbanForActivitySection: function() {
-				return this.getIsFeatureEnabled("EnableKanbanForActivitySection") 
+				return this.getIsFeatureEnabled("EnableKanbanForActivitySection") && false
 							&& this.entitySchemaName == "Activity";
 			},
 
@@ -1970,7 +1970,7 @@ define("KanbanSection", ["PageUtilities", "ConfigurationEnums"], function(PageUt
 			},
 
 			onElementDblClick: function(elementId) {
-				if (this.getIsFeatureEnabled("EnableKanbanForActivitySection") && this.hasMiniPage(this.entitySchemaName)) {
+				if (this.getIsFeatureEnabled("EnableKanbanForActivitySection") && false && this.hasMiniPage(this.entitySchemaName)) {
 					this.openEditMiniPage(elementId);
 				} else {
 					this.editRecord(elementId);
